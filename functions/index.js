4ff8d4028a7c9bc39b43d0a453844f3dda2bde87
sahilthakur7 <sahilrd07@gmail.com>
2020-06-09 16:24:49 +0530

    Wrote the business logic

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,41 @@
 const functions = require('firebase-functions');
+const axios = require('axios');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const articlesEndpoint = "https://dev.to/api/articles";
+
+async function getPost(postId, pageNo = 1) {
+    const { data } = axios.get(`${articlesEndpoint}/me/all?per_page=10&page=${pageNo}`,
+        {
+            headers: {
+                "api-key": functions.config().dev.api_key
+            }
+        }
+    );
+
+    const post = data.find((i) => i.id === postId);
+
+    if(!post){
+        getPost(postId, pageNo + 1);
+    }
+
+    return post;
+}
+
+async function updatePost(postId){
+    const { comments_count } = await getPost(postId);
+    const data = {title: `This post has ${comments_count} comments`};
+
+    const res = await axios.put(`${articlesEndpoint}/${postId}`, data, {
+        headers: {
+            "api-key": functions.config().dev.api_key,
+        },
+    });
+    return res.data;
+}
+
+exports.updatePostTitle = functions.pubsub
+    .schedule("every 3 minutes")
+    .onRun(() => {
+        const postId = functions.config().dev.post_id;
+        return updatePost(postId);
+    });
